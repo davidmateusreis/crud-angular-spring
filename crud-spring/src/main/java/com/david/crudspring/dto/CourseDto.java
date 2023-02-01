ae20b104cbf1500f4577b176df199d63b9cd5429
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2023-02-01 09:46:52 -0300

    CRUD Angular + Spring | 41: Padrão DTO (Data Transfer Object)

---
+++ crud-spring/src/main/java/com/david/crudspring/dto/CourseDto.java
@@ -0,0 +1,28 @@
+package com.david.crudspring.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
+@Data
+public class CourseDto {
+
+    @JsonProperty("_id")
+    private Long id; // DTO de response teria id, DTO de request, não
+
+    @NotBlank
+    @NotNull
+    @Length(min = 5, max = 100)
+    private String name;
+
+    @NotNull
+    @Length(max = 10)
+    @Pattern(regexp = "Backend|Frontend")
+    private String category;
+}
